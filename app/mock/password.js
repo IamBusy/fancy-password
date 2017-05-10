2ca96dcd4a594167f2484d2137cebfb9418211fd
william wei <1342247033@qq.com>
2017-05-10 22:28:36 +0800

    Add mock data for pwds

---
+++ app/mock/password.js
@@ -0,0 +1,16 @@
+/**
+ * Created by william on 10/05/2017.
+ */
+const Mock = require('mockjs');
+const Random = Mock.Random;
+let data = Mock.mock({
+    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    'data|10-15': [{
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        'id|+1': 1,
+        'name': () => Random.ctitle(2,4),
+        'account': () => Random.email(),
+        'password': () => Random.string('lower/upper/number/symbol',10,20)
+    }]
+})
+export default data.data;
--- app/pages/main.js
+++ app/pages/main.js
@@ -1,3 +1,71 @@
 /**
  * Created by william on 10/05/2017.
  */
+import React, { Component } from 'react';
+import SettingsList from 'react-native-settings-list';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {
+    Animated,
+    StyleSheet,
+    Platform,
+    StatusBar,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    TextInput,
+    Button
+} from 'react-native';
+
+import { randomIcon } from '../utils/random';
+import passwords from '../mock/password';
+
+
+export default class PwdItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <View style={{backgroundColor:'#EFEFF4',flex:1}}>
+                <View style={{borderBottomWidth:1, backgroundColor:'#f7f7f8',borderColor:'#c8c7cc'}}>
+                    <Text style={{alignSelf:'center',marginTop:30,marginBottom:10,fontWeight:'bold',fontSize:16}}>Settings</Text>
+                </View>
+                <View style={{backgroundColor:'#EFEFF4',flex:1}}>
+                    <SettingsList borderColor='#c8c7cc' defaultItemSize={60}>
+                        <SettingsList.Header headerStyle={{marginTop:15}}/>
+                        {
+                            passwords.map( pwd => (
+                                <SettingsList.Item
+                                    key={pwd.name}
+                                    icon={
+                                        <Icon name={'ios-'+randomIcon()} style={styles.imageStyle} size={35}  color="#4F8EF7" />
+                                    }
+                                    hasNavArrow={true}
+                                    title={pwd.name}
+                                    titleInfo={pwd.account}
+                                />
+                            ))
+                        }
+                    </SettingsList>
+                </View>
+            </View>
+        )
+    }
+
+
+}
+const styles = StyleSheet.create({
+    imageStyle:{
+        marginLeft:15,
+        alignSelf:'center',
+        height:30,
+        width:30
+    },
+    titleInfoStyle:{
+        fontSize:16,
+        color: '#8e8e93'
+    }
+});
