8694a3c89ab9ba16228ce5f1aed3eea941f8790c
william wei <1342247033@qq.com>
2017-05-10 22:48:44 +0800

    Add action button

--- app/pages/main.js
+++ app/pages/main.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import SettingsList from 'react-native-settings-list';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ActionButton from 'react-native-action-button';
 import {
     Animated,
     StyleSheet,
@@ -31,7 +32,7 @@ export default class PwdItem extends Component {
         return (
             <View style={{backgroundColor:'#EFEFF4',flex:1}}>
                 <View style={{borderBottomWidth:1, backgroundColor:'#f7f7f8',borderColor:'#c8c7cc'}}>
-                    <Text style={{alignSelf:'center',marginTop:30,marginBottom:10,fontWeight:'bold',fontSize:16}}>Settings</Text>
+                    <Text style={{alignSelf:'center',marginTop:20,marginBottom:10,fontWeight:'bold',fontSize:16}}></Text>
                 </View>
                 <View style={{backgroundColor:'#EFEFF4',flex:1}}>
                     <SettingsList borderColor='#c8c7cc' defaultItemSize={60}>
@@ -50,6 +51,14 @@ export default class PwdItem extends Component {
                             ))
                         }
                     </SettingsList>
+                    <ActionButton buttonColor="rgba(231,76,60,1)">
+                        <ActionButton.Item buttonColor='#9b59b6' title="Add Account" onPress={() => console.log("notes tapped!")}>
+                            <Icon name="md-create" style={styles.actionButtonIcon} />
+                        </ActionButton.Item>
+                        <ActionButton.Item buttonColor='#3498db' title="Scan" onPress={() => {}}>
+                            <Icon name="md-qr-scanner" style={styles.actionButtonIcon} />
+                        </ActionButton.Item>
+                    </ActionButton>
                 </View>
             </View>
         )
@@ -67,5 +76,10 @@ const styles = StyleSheet.create({
     titleInfoStyle:{
         fontSize:16,
         color: '#8e8e93'
-    }
+    },
+    actionButtonIcon: {
+        fontSize: 20,
+        height: 22,
+        color: 'white',
+    },
 });
