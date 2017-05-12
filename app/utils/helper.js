d9ae2bb4d11ecf3f60792b9cea2450be450a2212
william wei <1342247033@qq.com>
2017-05-12 15:24:34 +0800

    Remove exception

--- app/utils/helper.js
+++ app/utils/helper.js
@@ -4,13 +4,11 @@
 export function runOnce(fn, context) { //控制让函数只触发一次
     return function () {
         try {
-            if( fn ) {
-                fn.apply(context || this, arguments);
-            }
+            fn.apply(context || this, arguments);
 
         }
         catch (e) {
-            console.error(e);//一般可以注释掉这行
+            //console.error(e);//一般可以注释掉这行
         }
         finally {
             fn = null;
