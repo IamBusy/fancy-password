d2a13f140b20bfd113fefbbfa272ccc02c78a20c
william wei <1342247033@qq.com>
2017-06-17 11:15:51 +0800

    Updata api

--- app/env.js
+++ app/env.js
@@ -2,7 +2,7 @@
  * Created by william on 10/05/2017.
  */
 export default {
-    api     : 'http://devapi.umeishu.com/v1/',
+    api     : 'http://139.224.236.65/hci/',
     version : '1.0.0',
     database_name: 'fancy-password',
     database_info: 'fancy-password-info',
