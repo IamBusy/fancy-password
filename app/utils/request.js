9ccd61d3eacda66221430ea13275bb7529c6fc4f
william wei <1342247033@qq.com>
2017-06-17 10:24:54 +0800

    Add request

---
+++ app/utils/request.js
@@ -0,0 +1,74 @@
+import env from '../env';
+
+function convert(obj) {
+    let query = '',
+        name, value, fullSubName, subName, subValue, innerObj, i;
+    for (name in obj) {
+        value = obj[name];
+        if (value instanceof Array) {
+            for (i = 0; i < value.length; ++i) {
+                subValue = value[i];
+                fullSubName = name + '[' + i + ']';
+                innerObj = {};
+                innerObj[fullSubName] = subValue;
+                query += param(innerObj) + '&';
+            }
+        } else if (value instanceof Object) {
+            for (subName in value) {
+                subValue = value[subName];
+                fullSubName = name + '[' + subName + ']';
+                innerObj = {};
+                innerObj[fullSubName] = subValue;
+                query += param(innerObj) + '&';
+            }
+        } else if (value !== undefined && value !== null) query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+    return query.length ? query.substr(0, query.length - 1) : query;
+}
+
+function parseJSON(response) {
+    return response.json();
+}
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+}
+
+/**
+ * Requests a URL, returning a promise.
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ * @return {object}           An object containing either "data" or "err"
+ */
+function request(url, options) {
+
+    let headers = {
+        "Content-Type": "application/json",
+    };
+
+    //url = 'api'+url;
+    url = env.api + url;
+
+    options = { ...options, headers, body: JSON.stringify(options.body) };
+    //console.log(options);
+
+    return fetch(url, options)
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(data => ( data.data?data.data:data ))
+        .catch(err => ({ err }));
+}
+
+export default {
+    get:  (url,data)=>request(url,{ body:(data),method:"get"}),
+    post: (url,data)=>request(url,{ body:(data),method:"post"}),
+    put:  (url,data)=>request(url,{ body:(data),method:"put"}),
+    delete:(url,data)=>request(url,{body:(data),method:"delete"}),
+}
